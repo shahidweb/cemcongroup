3d0ea3353faa95202e5972c415f863bc5dea8f4a
Shahid Khan <shahidsaifi88@gmail.com>
2023-09-22 15:50:24 +0530

    removeLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componets/home/home.component';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
