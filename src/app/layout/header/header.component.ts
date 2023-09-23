0b1d22ec5f62a123227bfcce2fd3975d0a702e10
Shahid Khan <shahidsaifi88@gmail.com>
2023-09-23 17:08:56 +0530

    changeHeaderwithResponsive

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {  
+export class HeaderComponent {
+  title = 'CEMCON GROUP';
+  navbar: any = [
+    { type: 'text', path: '/', name: 'Home', },
+    { type: 'text', path: '/', name: 'About us', },
+    {
+      type: 'text', path: '/', name: 'Product', subMenu: [
+        { path: '/', name: 'Track Fitting' },
+        { path: '/', name: 'Carriage and Wagon Stock' },
+      ]
+    },
+    { type: 'text', path: '/', name: 'Career', },
+    { type: 'text', path: '/', name: 'Contact', },
+    { type: 'btn', path: '/', name: 'Request Quote', },
+  ]
 
 }
