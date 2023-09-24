a4e2393223fd398c7bcf6942e4aa0d68f0504211
Shahid Khan <shahidsaifi88@gmail.com>
2023-09-24 16:12:13 +0530

    changinghomePage

---
+++ src/app/common/core.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoreService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getHttp() {
+    return this.http.get(environment.apiUrl);
+  }
+}
--- src/app/componets/home/home.component.ts
+++ src/app/componets/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoreService } from 'src/app/common/core.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  data: any = []
+  constructor(private coreService: CoreService) { }
+
+  ngOnInit(): void {
+    this.getSliderData();
+  }
+
+  getSliderData() {
+    this.coreService.getHttp().subscribe((res:any) => {
+      this.data = res.slider;
+      console.log(this.data)
+    })
+  }
+
+
 
 }
